8efa15473f8200f6ae2b6a5fd6f158027ff15581
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-07-21 11:30:14 -0400

    $1

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -42,9 +42,9 @@ html_theme_options = {
         "repo": "fontawesome/brands/github",
         "edit": "material/file-edit-outline",
     },
-    "site_url": "https://jbms.github.io/sphinx-immaterial/",
-    "repo_url": "https://github.com/jbms/sphinx-immaterial/",
-    "repo_name": "Sphinx-Immaterial",
+#    "site_url": "https://jbms.github.io/sphinx-immaterial/",
+#    "repo_url": "https://github.com/jbms/sphinx-immaterial/",
+#    "repo_name": "Sphinx-Immaterial",
     "edit_uri": "blob/main/docs",
     "globaltoc_collapse": True,
     "features": [
@@ -87,32 +87,32 @@ html_theme_options = {
     ],
     # BEGIN: version_dropdown
     "version_dropdown": True,
-    "version_info": [
-        {
-            "version": "https://sphinx-immaterial.rtfd.io",
-            "title": "ReadTheDocs",
-            "aliases": [],
-        },
-        {
-            "version": "https://jbms.github.io/sphinx-immaterial",
-            "title": "Github Pages",
-            "aliases": [],
-        },
-    ],
+#   "version_info": [
+#       {
+#           "version": "https://sphinx-immaterial.rtfd.io",
+#           "title": "ReadTheDocs",
+#           "aliases": [],
+#       },
+#       {
+#           "version": "https://jbms.github.io/sphinx-immaterial",
+#           "title": "Github Pages",
+#           "aliases": [],
+#       },
+#   ],
     # END: version_dropdown
     "toc_title_is_page_title": True,
     # BEGIN: social icons
-    "social": [
-        {
-            "icon": "fontawesome/brands/github",
-            "link": "https://github.com/jbms/sphinx-immaterial",
-            "name": "Source on github.com",
-        },
-        {
-            "icon": "fontawesome/brands/python",
-            "link": "https://pypi.org/project/sphinx-immaterial/",
-        },
-    ],
+#   "social": [
+#       {
+#           "icon": "fontawesome/brands/github",
+#           "link": "https://github.com/jbms/sphinx-immaterial",
+#           "name": "Source on github.com",
+#       },
+#       {
+#           "icon": "fontawesome/brands/python",
+#           "link": "https://pypi.org/project/sphinx-immaterial/",
+#       },
+#   ],
     # END: social icons
 }
 
