e5f1adcbc6bb9bb3950e15b03ea5ab8f8b26e7d6
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2023-08-07 16:02:17 -0400

    $1

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ release = '0.8'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['myst_parser', 'sphinx_immaterial']
 
 myst_enable_extensions = [
   "colon_fence", "deflist"
@@ -36,5 +36,80 @@ source_suffix = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'piccolo_theme'
+html_theme = 'sphinx_immaterial'
+
+extensions.append("sphinx_immaterial")
+
+# material theme options (see theme.conf for more information)
+html_theme_options = {
+    "icon": {
+        "repo": "fontawesome/brands/github",
+        "edit": "material/file-edit-outline",
+    },
+#    "site_url": "https://jbms.github.io/sphinx-immaterial/",
+#    "repo_url": "https://github.com/jbms/sphinx-immaterial/",
+#    "repo_name": "Sphinx-Immaterial",
+    "edit_uri": "blob/main/docs",
+    "globaltoc_collapse": True,
+    "features": [
+        "navigation.expand",
+        # "navigation.tabs",
+        # "toc.integrate",
+        "navigation.sections",
+        # "navigation.instant",
+        # "header.autohide",
+        "navigation.top",
+        # "navigation.tracking",
+        # "search.highlight",
+        "search.share",
+        "toc.follow",
+        "toc.sticky",
+        "content.tabs.link",
+        "announce.dismiss",
+    ],
+    "palette": [
+        {
+            "media": "(prefers-color-scheme: light)",
+            "scheme": "default",
+            "primary": "light-green",
+            "accent": "light-blue",
+            "toggle": {
+                "icon": "material/lightbulb-outline",
+                "name": "Switch to dark mode",
+            },
+        },
+        {
+            "media": "(prefers-color-scheme: dark)",
+            "scheme": "slate",
+            "primary": "deep-orange",
+            "accent": "lime",
+            "toggle": {
+                "icon": "material/lightbulb",
+                "name": "Switch to light mode",
+            },
+        },
+    ],
+    "toc_title_is_page_title": True,
+    # BEGIN: social icons
+}
+
+# Options for sphinx_material
+#html_theme_options = {
+#   'base_url': 'http://kurt-krueckeberg.io/',
+#   'repo_url': 'https://github.com/kurt-krueckeberg/kurt-krueckeberg.github.io/',
+#   'html_minify': True,
+#   'css_minify': True,
+#   'logo_icon': '&#xe869',
+#   'globaltoc_depth': 2,
+##    "color_primary": "blue",
+##    "color_accent": "cyan",
+##    "theme_color": "#2196f3",
+##    "version_dropdown": True,
+##    "table_classes": ["plain"]
+#}
+
 html_static_path = ['_static']
+
+html_css_files = [
+    'kurt-style.css',
+]
